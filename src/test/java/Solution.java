19d7b4c44e437b608f323c302df793b482972ea6
杨淮生 <codefan@centit.com>
2018-12-12 08:47:52 +0800

    添加 分糖 算法

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -8,63 +8,51 @@ public class Solution {
             doubleNumbles[i] = (double) nums[i];
         }
 
-        return  _judgePoint24(doubleNumbles, doubleNumbles.length);
+        return  _judgePoint(doubleNumbles, doubleNumbles.length, 24);
     }
 
-    public  boolean _judgePoint24(double[] nums, int n) {
+    public  boolean _judgePoint(double[] nums, int n, int sum) {
         if (n == 1) {
-            return (Math.abs(nums[0] - 24)) < DIFF;
+            return (Math.abs(nums[0] - sum)) < DIFF;
         }
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 double a = nums[i];
                 double b = nums[j];
-
-
                 //每计算一次，参与运算的数字就少一个。将最后一个数字复制到位置j,可达到缩小（缩短）数组的效果
                 nums[j] = nums[n - 1];
-
-
                 //加法操作 a+b
                 nums[i] = a + b;
-                if (_judgePoint24(nums, n - 1)) {
+                if (_judgePoint(nums, n - 1, sum)) {
                     return true;
                 }
-
                 //减法操作 a-b
                 nums[i] = a - b;
-                if (_judgePoint24(nums, n - 1)) {
+                if (_judgePoint(nums, n - 1, sum)) {
                     return true;
                 }
-
                 //减法操作 b-a
                 nums[i] = b - a;
-                if (_judgePoint24(nums, n - 1)) {
+                if (_judgePoint(nums, n - 1, sum)) {
                     return true;
                 }
-
                 //乘法操作 a*b
                 nums[i] = a * b;
-                if (_judgePoint24(nums, n - 1)) {
+                if (_judgePoint(nums, n - 1, sum)) {
                     return true;
                 }
-
                 //除法操作 a/b, 如果除数不为0
-                if (b != 0) {
+                if (b != 0 && a != 0) {
                     nums[i] = a / b;
-                    if (_judgePoint24(nums, n - 1)) {
+                    if (_judgePoint(nums, n - 1, sum)) {
                         return true;
                     }
-                }
-
-                //除法操作 b/a , 如果除数不为0
-                if (a != 0) {
                     nums[i] = b / a;
-                    if (_judgePoint24(nums, n - 1)) {
+                    if (_judgePoint(nums, n - 1, sum)) {
                         return true;
                     }
                 }
-
+                // 恢复数组
                 nums[i] = a;
                 nums[j] = b;
             }
@@ -72,11 +60,39 @@ public class Solution {
         return false;
     }
 
+    public long allocateCandy(int kids, int candies){
+        if(kids<=1){
+            return 1;
+        }
+        if(candies==1){
+            return kids;
+        }
+        long temp[] = new long[kids];
+        for(int i=0; i<kids; i++){
+            temp[i] = (long)(i+1);
+        }
+        long res = temp[kids-1];
+        for(int i=1; i<candies; i++){
+            res = 1;
+            for(int j = 1; j<kids; j++){
+                res +=  temp[j];
+                temp[j] = res;
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = { 3, 3, 8, 8 };
-        System.out.println(s.judgePoint24(nums));
+        //int[] nums = { 3, 3, 8, 8 };
+        System.out.println(s.allocateCandy(5,6));
 
     }
 }
-
+// 18851684897584
+// 1397281501935165
+// 6365767297450757748
+// 6009637891325159524
+// 1837153897788780084
+// 8115943777403887392
+// 6650043942411187488
